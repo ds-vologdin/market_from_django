23fd878da35d831584a7a9e5579e757230bfdd0f
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-06-01 10:36:09 +0300

    Добавил базовый шаблон с выводом категорий продуктов

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,21 @@
 from django.views import generic
 
-from .models import Product
+from .models import Product, MainCategoryProduct, CategoryProduct
+
+
+def get_categorys():
+    # TODO: Подумать над кешированием этих запросов
+    main_categorys = MainCategoryProduct.objects.all()
+    categorys = [
+        {
+            'main': main_category,
+            'sub': CategoryProduct.objects.filter(
+                main_category_id__exact=main_category.id
+            ),
+        }
+        for main_category in main_categorys
+    ]
+    return categorys
 
 
 # Create your views here.
@@ -11,7 +26,21 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Product.objects.all().order_by('-rating', 'cost')[:5]
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update({
+            'categorys': get_categorys(),
+        })
+        return context
+
 
 class DetailView(generic.DetailView):
     model = Product
     template_name = 'catalog/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        context.update({
+            'categorys': get_categorys(),
+        })
+        return context
