6ad790f849c2f4fecfdbf3ac47995f1ae52fe7c4
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-05 16:24:09 +0300

    Подключил django-extensions

--- market/settings.py
+++ market/settings.py
@@ -14,6 +14,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'catalog.apps.CatalogConfig',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
