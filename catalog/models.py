8a9cdfcc24dffc6021a2c0b9aec93867f1000f06
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-07-05 16:13:12 +0300

    Удалил jsonb параметр + фикстуры

--- catalog/models.py
+++ catalog/models.py
@@ -27,8 +27,6 @@ class Product(models.Model):
     cost = models.DecimalField(max_digits=12, decimal_places=2, blank=True)
     rating = models.IntegerField()
     category = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE)
-    # TODO: предусмотреть размещение параметров продукта в jsonb
-    parameters = JSONField(default={})
 
     def __str__(self):
         return self.name
